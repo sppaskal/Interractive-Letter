b10663a5a405b2db0ead169f8ef0a3753b0dbfa4
sppaskal <sppaskal@users.noreply.github.com>
2023-12-01 12:16:39 -0700

    Update main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# Program interraction loop
+def main():
+
+    option = 0
+    while option != 9:
+        print("")
+        print("        WELCOME DIANA")
+        print("=============================")
+        print("(1) - Compliment")
+        print("(2) - A favorite memory")
+        print("(3) - A reason I love you")
+        print("(9) - QUIT")
+
+        option = int(input("Type your choice and press enter: "))
+
+        print("She chose: " + str(option))
+
+
+main()
