7c2511fc6dcb1cc700a92f506631e2776be4a18d
sppaskal <sppaskal@users.noreply.github.com>
2023-12-01 20:53:22 -0700

    Adding jokes logic and json
    Also small text changes to memory prompt

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ reasons = Utils.convert_dict_keys_to_ints(
     )
 )
 
+used_jokes = []
+jokes = Utils.convert_dict_keys_to_ints(
+    Utils.load_json_as_dict(
+        path="data/jokes.json"
+    )
+)
+
 # -------------------------------------------------------------------------------
 
 
@@ -31,8 +38,9 @@ def main():
 
     while True:
         print("(1) - Compliment")
-        print("(2) - A favorite memory")
+        print("(2) - Treasured memory")
         print("(3) - A reason I love you")
+        print("(4) - Dad joke")
         print("(0) - QUIT")
 
         choice = int(input("Type your choice and press enter: "))
@@ -55,6 +63,7 @@ def switch_case(case_key):
             1: get_compliment,
             2: get_memory,
             3: get_reason,
+            4: get_joke,
         }
 
     case_function = cases.get(case_key, get_default)
@@ -110,6 +119,22 @@ def get_reason():
 
     text = reasons.get(random_key)
     used_reasons.append(random_key)
+
+    return text
+
+
+def get_joke():
+    random_key = Utils.get_random_integer_in_range(
+        min=0,
+        max=len(jokes),
+        exclusions=used_jokes
+    )
+
+    if random_key is None:
+        return "Looks like I'm out of jokes for now..."
+
+    text = jokes.get(random_key)
+    used_jokes.append(random_key)
 
     return text
 
