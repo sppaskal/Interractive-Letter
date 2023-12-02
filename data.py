2791b2b476c1c0b88157029f4be211dfd92537df
sppaskal <sppaskal@users.noreply.github.com>
2023-12-01 23:20:22 -0700

    Refactoring main and extracting alot of functionality into Data class
    Created a Data class that holds all the text data and methods that access and edit that data

---
+++ data.py
@@ -0,0 +1,167 @@
+from utils import Utils
+
+
+class Data():
+
+    def __init__(self):
+        self.used_compliments = []
+        self.compliments = Utils.convert_dict_keys_to_ints(
+            Utils.load_json_as_dict(
+                path="data/compliments.json"
+            )
+        )
+
+        self.used_memories = []
+        self.memories = Utils.convert_dict_keys_to_ints(
+            Utils.load_json_as_dict(
+                path="data/memories.json"
+            )
+        )
+
+        self.used_reasons = []
+        self.reasons = Utils.convert_dict_keys_to_ints(
+            Utils.load_json_as_dict(
+                path="data/reasons.json"
+            )
+        )
+
+        self.used_jokes = []
+        self.jokes = Utils.convert_dict_keys_to_ints(
+            Utils.load_json_as_dict(
+                path="data/jokes.json"
+            )
+        )
+
+        self.data_types = {
+            0: "DNE",
+            1: "compliments",
+            2: "memories",
+            3: "reasons",
+            4: "jokes"
+        }
+
+    # -----------------------------------------------------
+
+    def get_default(self):
+        text = "I'm not familiar with that option. Try one of the options I listed!"
+        return {
+                "text": text,
+                "found_new": True,
+                "type": self.data_types.get(0)
+            }
+
+    # -----------------------------------------------------
+
+    def get_compliment(self):
+        random_key = Utils.get_random_integer_in_range(
+            min=0,
+            max=len(self.compliments),
+            exclusions=self.used_compliments
+        )
+
+        if random_key is None:
+            text = "Looks like I'm out of compliments for now..."
+            return {
+                "text": text,
+                "found_new": False,
+                "type": self.data_types.get(1)
+            }
+
+        text = self.compliments.get(random_key)
+        self.used_compliments.append(random_key)
+
+        return {
+            "text": text,
+            "found_new": True,
+            "type": self.data_types.get(1)
+        }
+
+    # -----------------------------------------------------
+
+    def get_memory(self):
+        random_key = Utils.get_random_integer_in_range(
+            min=0,
+            max=len(self.memories),
+            exclusions=self.used_memories
+        )
+
+        if random_key is None:
+            text = "Looks like I'm out of memories for now..."
+            return {
+                "text": text,
+                "found_new": False,
+                "type": self.data_types.get(2)
+            }
+
+        text = self.memories.get(random_key)
+        self.used_memories.append(random_key)
+
+        return {
+            "text": text,
+            "found_new": True,
+            "type": self.data_types.get(2)
+        }
+
+    # -----------------------------------------------------
+
+    def get_reason(self):
+        random_key = Utils.get_random_integer_in_range(
+            min=0,
+            max=len(self.reasons),
+            exclusions=self.used_reasons
+        )
+
+        if random_key is None:
+            text = "Looks like I'm out of reasons for now..."
+            return {
+                "text": text,
+                "found_new": False,
+                "type": self.data_types.get(3)
+            }
+
+        text = self.reasons.get(random_key)
+        self.used_reasons.append(random_key)
+
+        return {
+            "text": text,
+            "found_new": True,
+            "type": self.data_types.get(3)
+        }
+
+    # -----------------------------------------------------
+
+    def get_joke(self):
+        random_key = Utils.get_random_integer_in_range(
+            min=0,
+            max=len(self.jokes),
+            exclusions=self.used_jokes
+        )
+
+        if random_key is None:
+            text = "Looks like I'm out of jokes for now..."
+            return {
+                "text": text,
+                "found_new": False,
+                "type": self.data_types.get(4)
+            }
+
+        text = self.jokes.get(random_key)
+        self.used_jokes.append(random_key)
+
+        return {
+            "text": text,
+            "found_new": True,
+            "type": self.data_types.get(4)
+        }
+
+    # -------------------------------------------------------------------------------
+
+    def reset_data(self, type):
+        if type == self.data_types.get(1):
+            self.used_compliments = []
+        elif type == self.data_types.get(2):
+            self.used_memories = []
+        elif type == self.data_types.get(3):
+            self.used_reasons = []
+        elif type == self.data_types.get(4):
+            self.used_jokes = []
--- main.py
+++ main.py
@@ -1,48 +1,12 @@
-from utils import Utils
-
-used_compliments = []
-compliments = Utils.convert_dict_keys_to_ints(
-    Utils.load_json_as_dict(
-        path="data/compliments.json"
-    )
-)
-
-used_memories = []
-memories = Utils.convert_dict_keys_to_ints(
-    Utils.load_json_as_dict(
-        path="data/memories.json"
-    )
-)
-
-used_reasons = []
-reasons = Utils.convert_dict_keys_to_ints(
-    Utils.load_json_as_dict(
-        path="data/reasons.json"
-    )
-)
-
-used_jokes = []
-jokes = Utils.convert_dict_keys_to_ints(
-    Utils.load_json_as_dict(
-        path="data/jokes.json"
-    )
-)
-
-data_types = {
-    0: "DNE",
-    1: "compliments",
-    2: "memories",
-    3: "reasons",
-    4: "jokes"
-}
-
-# -------------------------------------------------------------------------------
+from data import Data
 
 
 # Program interraction loop
 def main():
     print("        WELCOME DIANA")
     print("=============================")
+
+    data = Data()
 
     while True:
         print("(1) - Compliment")
@@ -56,7 +20,7 @@ def main():
         if choice == 0:
             break
 
-        response = switch_case(choice)
+        response = switch_case(data, choice)
         text_resp = response.get("text")
         found_new = response.get("found_new")
         type = response.get("type")
@@ -66,7 +30,7 @@ def main():
         if not found_new:
             reset = input("Would you like to reset " + str(type) + "? (Y/N): ")
             if reset.lower() == "y":
-                reset_data(type)
+                data.reset_data(type)
 
         print("")
         print("=============================")
@@ -75,140 +39,19 @@ def main():
 # -------------------------------------------------------------------------------
 
 
-def switch_case(case_key):
+def switch_case(data, case_key):
     cases = {
-            1: get_compliment,
-            2: get_memory,
-            3: get_reason,
-            4: get_joke,
+            1: data.get_compliment,
+            2: data.get_memory,
+            3: data.get_reason,
+            4: data.get_joke,
         }
 
-    case_function = cases.get(case_key, get_default)
+    case_function = cases.get(case_key, data.get_default)
 
     return case_function()
 
 
-def get_default():
-    text = "I'm not familiar with that option. Try one of the options I listed!"
-    return {
-            "text": text,
-            "found_new": True,
-            "type": data_types.get(0)
-        }
-
-
-def get_compliment():
-    random_key = Utils.get_random_integer_in_range(
-        min=0,
-        max=len(compliments),
-        exclusions=used_compliments
-    )
-
-    if random_key is None:
-        text = "Looks like I'm out of compliments for now..."
-        return {
-            "text": text,
-            "found_new": False,
-            "type": data_types.get(1)
-        }
-
-    text = compliments.get(random_key)
-    used_compliments.append(random_key)
-
-    return {
-        "text": text,
-        "found_new": True,
-        "type": data_types.get(1)
-    }
-
-
-def get_memory():
-    random_key = Utils.get_random_integer_in_range(
-        min=0,
-        max=len(memories),
-        exclusions=used_memories
-    )
-
-    if random_key is None:
-        text = "Looks like I'm out of memories for now..."
-        return {
-            "text": text,
-            "found_new": False,
-            "type": data_types.get(2)
-        }
-
-    text = memories.get(random_key)
-    used_memories.append(random_key)
-
-    return {
-        "text": text,
-        "found_new": True,
-        "type": data_types.get(2)
-    }
-
-
-def get_reason():
-    random_key = Utils.get_random_integer_in_range(
-        min=0,
-        max=len(reasons),
-        exclusions=used_reasons
-    )
-
-    if random_key is None:
-        text = "Looks like I'm out of reasons for now..."
-        return {
-            "text": text,
-            "found_new": False,
-            "type": data_types.get(3)
-        }
-
-    text = reasons.get(random_key)
-    used_reasons.append(random_key)
-
-    return {
-        "text": text,
-        "found_new": True,
-        "type": data_types.get(3)
-    }
-
-
-def get_joke():
-    random_key = Utils.get_random_integer_in_range(
-        min=0,
-        max=len(jokes),
-        exclusions=used_jokes
-    )
-
-    if random_key is None:
-        text = "Looks like I'm out of jokes for now..."
-        return {
-            "text": text,
-            "found_new": False,
-            "type": data_types.get(4)
-        }
-
-    text = jokes.get(random_key)
-    used_jokes.append(random_key)
-
-    return {
-        "text": text,
-        "found_new": True,
-        "type": data_types.get(4)
-    }
-
 # -------------------------------------------------------------------------------
-
-def reset_data(type):
-    if type == data_types.get(1):
-        used_compliments = []
-    elif type == data_types.get(2):
-        used_memories = []
-    elif type == data_types.get(3):
-        used_reasons = []
-    elif type == data_types.get(4):
-        used_jokes = []
-
-# -------------------------------------------------------------------------------
-
 
 main()
